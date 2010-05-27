084b83b38d861c6ae1ae6aa31a31fd0b5b1c73d0
grosser <grosser.michael@gmail.com>
2010-05-27 11:54:55 +0200

    inital

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.beta3'
+
+gem "rspec-rails", ">= 2.0.0.beta.8", :group => :test
+gem "factory_girl", ">= 1.2.4", :group => :test
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '93a574b4a72c55997a069ae56347ff5d5dd0d8493192702cd511a002ce44ed59707a9b659a53da091d1ae2211b22c3e809801de5efeecd74aec2283d73473b0a'
