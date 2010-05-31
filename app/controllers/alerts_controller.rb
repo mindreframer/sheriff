0b268261cba4184206573aff08053b16e647dd2d
grosser <grosser.michael@gmail.com>
2010-05-31 17:36:56 +0200

    sort alerts

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,3 +1,7 @@
 class AlertsController < RestController
   layout 'group_sidebar'
+
+  def current_objects
+    @current_objects = current_model.paginate(:per_page => 20, :page => params[:page], :order => 'id desc')
+  end
 end
