89243de9f313fc1bcf6bb58ef0befaf0ad7a2358
grosser <grosser.michael@gmail.com>
2010-06-04 21:34:59 +0200

    pagination and some includes for speed

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -2,6 +2,6 @@ class AlertsController < RestController
   layout 'group_sidebar'
 
   def collection
-    @collection ||= resource_class.paginate(:per_page => 20, :page => params[:page], :order => 'id desc')
+    @collection ||= resource_class.paginate(:per_page => 20, :page => params[:page], :order => 'id desc', :include => {:report => [{:group => :group}, :deputy]})
   end
 end
