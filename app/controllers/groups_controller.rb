0195ee9f2ba56b827feca7a0112ba18763ce63d9
grosser <grosser.michael@gmail.com>
2010-06-05 20:10:36 +0200

    add heading for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,6 @@ class GroupsController < RestController
   layout 'group_sidebar'
 
   def resource
-    @group ||= Group.find(params[:id], :include => {:reports => :validations})
+    @resource ||= @group ||= Group.find(params[:id], :include => {:reports => :validations})
   end
 end
