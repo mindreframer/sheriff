529e7b4c7b097d35e0088a40d5dcae24c48d2e39
grosser <grosser.michael@gmail.com>
2010-06-11 14:13:06 +0200

    pagination for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,6 @@ class GroupsController < RestController
   layout 'group_sidebar'
 
   def resource
-    @resource ||= @group ||= Group.find(params[:id], :include => {:reports => :validations})
+    @resource ||= @group ||= Group.find(params[:id], :include => {:reports => :validations}, :order => :group_id)
   end
 end
