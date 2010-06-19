c770cb1797be9d0a4878a176d6e35f5977b49a40
grosser <grosser.michael@gmail.com>
2010-06-19 07:05:51 +0200

    add time to mails

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -4,6 +4,6 @@ class Notifier < ActionMailer::Base
 
   def alert(alert)
     mail :subject => "Sheriff Error -- #{alert.report.full_name}",
-      :body => alert.message
+      :body => "#{alert.message}\n#{Time.current.to_s(:db)} UTC"
   end
 end
