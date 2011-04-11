253d4ccf099770add45cebfc54a4d180d8d525ff
grosser <grosser.michael@gmail.com>
2011-04-11 21:49:19 +0200

    read config.yml from heroku env

--- lib/cfg.rb
+++ lib/cfg.rb
@@ -1,2 +1,8 @@
 env = defined?(Rails.env) ? Rails.env : (ENV['RAILS_ENV'] || 'development')
-CFG = YAML.load_file('config/config.yml')[env].with_indifferent_access.freeze
+config = if ENV['CONFIG.YML']
+  require 'base64'
+  Base64.decode64(ENV['CONFIG.YML'])
+else
+  File.read('config/config.yml')
+end
+CFG = YAML.load(config)[env].with_indifferent_access.freeze
