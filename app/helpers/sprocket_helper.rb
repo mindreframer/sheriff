41383a94dbfb0cacb378a3987f36f5b0c898b925
Roman Heinrich <roman.heinrich@gmail.com>
2012-02-26 23:34:13 +0100

    partial for sprockets (not locals) works

--- app/helpers/sprocket_helper.rb
+++ app/helpers/sprocket_helper.rb
@@ -1,10 +1,24 @@
+# is included in Rails.application.assets.context_class.class_eval
 module SprocketHelper
-  def hallo
-    "hallo du"
+  POSSIBLE_FORMATS = ["html", 'erb']
+  ALL_PERMUTATIONS = POSSIBLE_FORMATS.permutation.to_a + POSSIBLE_FORMATS.map{|x|[x]}
+
+  # http://ididitmyway.heroku.com/past/2010/5/31/partials/
+  ### redid as very basic version
+  def partial(template,locals=nil)
+    # prepend basename with underscore
+    template = File.join(File.dirname(template.to_s), '_' + File.basename(template.to_s))
+    # render
+    erb (template, {}, locals)
+  end
+
+  def erb(template_path, options={}, locals=nil)
+    base_path        = Rails.root.join("app/assets/templates")
+    file_path        = base_path.join(template_path.to_s)
+    possible_files   = ALL_PERMUTATIONS.map{|e| "#{file_path}.#{e.join('.')}"}
+    possible_files.reject!{|x| not File.exists?(x)}
+    content          = File.read(possible_files.first)
+    template         = ERB.new(content)
+    template.result(binding)
   end
 end
-
-# Rails.application.assets.context_class.class_eval do
-#   include ActionView::Helpers
-#   include Rails.application.routes.url_helpers
-# end
