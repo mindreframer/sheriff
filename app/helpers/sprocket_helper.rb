3a54cf98c610bf34ea164609456f2761ac4cf920
Roman Heinrich <roman.heinrich@gmail.com>
2012-02-26 22:37:31 +0100

    sprocket helper

---
+++ app/helpers/sprocket_helper.rb
@@ -0,0 +1,10 @@
+module SprocketHelper
+  def hallo
+    "hallo du"
+  end
+end
+
+# Rails.application.assets.context_class.class_eval do
+#   include ActionView::Helpers
+#   include Rails.application.routes.url_helpers
+# end
---
+++ lib/ext/sprockets.rb
@@ -0,0 +1,5 @@
+Rails.application.assets.context_class.class_eval do
+  #include ActionView::Helpers
+  #include Rails.application.routes.url_helpers
+  include SprocketHelper
+end
