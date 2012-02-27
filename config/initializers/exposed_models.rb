5c2055562c662826c664bf249530d7409251883f
Roman Heinrich <roman.heinrich@gmail.com>
2012-02-27 03:02:15 +0100

    more params for model definition in js

--- config/initializers/exposed_models.rb
+++ config/initializers/exposed_models.rb
@@ -1,1 +1,4 @@
-Sheriff::EXPOSED_MODELS = ['Alert', 'Deputy', 'Group', 'Plugin', 'Report', 'Settings']
+Sheriff::EXPOSED_MODELS = [Alert, Deputy, Group, Plugin, Report, Settings].map do |m|
+  {:model => m.name,
+  :per_page => m.respond_to?('per_page') ? m.per_page : 50}
+end
