67075a91e35181409e8a23fa9252bc6d58d60ea8
Paul Asmuth <paul@paulasmuth.com>
2012-06-18 15:30:37 +0200

    moved fyrehose report hook to alert model, use udp

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -2,6 +2,7 @@ class Alert < ActiveRecord::Base
   belongs_to :validation, :polymorphic => true
   belongs_to :report
   after_create :send_notification
+  after_create :report_to_fyrehose
   before_create :adjust_validation_type
 
   protected
@@ -23,4 +24,25 @@ class Alert < ActiveRecord::Base
   def send_mail
     Notifier.alert(self).deliver
   end
+
+  def report_to_fyrehose
+    return true unless CFG[:report_to_fyrehose]
+    msg = "#{report.full_name} - #{message}"
+
+    pub = if error_level == 0
+      { :token => "sherrif.#{self.report.group.full_name}", :type => "resolved" }
+    else
+      { :token => "sherrif.#{self.report.group.full_name}", :type => "issue", :message => msg, :priority => "high" }
+    end
+
+    pub.merge!(:channel => CFG[:report_to_fyrehose_channel])
+
+    sock = UDPSocket.new
+    sock.connect(*CFG[:report_to_fyrehose].split(":"))
+    sock.write(pub.to_json + "\n")
+    sock.close
+  rescue Exception => e
+    puts "cant report to fyrehose: #{e.to_s}"
+  end
+
 end
