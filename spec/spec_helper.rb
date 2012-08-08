2a717e4626e3a4f992cb96a23244b8f95f350e7a
Roman Heinrich <roman.heinrich@gmail.com>
2012-08-08 17:39:39 +0300

    some path issues on ruby19

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 ENV["RAILS_ENV"] ||= 'test'
+### see: http://www.scottw.com/ruby-1-9-2-load
+$: << '.'
+
 require File.dirname(__FILE__) + "/../config/environment" unless defined?(Rails)
 require 'rspec/rails'
 
