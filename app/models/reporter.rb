3fd893371a558baf62ee9b879ce10c9a2807270d
Roman Heinrich <roman.heinrich@gmail.com>
2012-10-22 20:24:43 +0300

    new class: Reporter

---
+++ app/models/reporter.rb
@@ -0,0 +1,19 @@
+class Reporter
+  def self.single_notification(alert)
+    case alert.error_level
+    when 2 then
+      Reporter.send_mail(alert)
+    when 3 then
+      Reporter.send_mail(alert)
+      Reporter.send_sms(alert)
+    end
+  end
+
+  def self.send_mail(alert)
+    SheriffMailer.alert(alert).deliver
+  end
+
+  def self.send_sms(alert)
+    Sms.send "#{alert.report.full_name} - #{alert.message}", CFG[:sms_recipients].to_s.split(',')
+  end
+end
