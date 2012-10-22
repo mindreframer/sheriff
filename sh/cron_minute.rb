967401fb020af71e683f6ef5a2e1ee1302dbb49e
Roman Heinrich <roman.heinrich@gmail.com>
2012-10-23 00:15:35 +0300

    cron minute with working settings[] stuff

--- sh/cron_minute.rb
+++ sh/cron_minute.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'config/environment'
 puts Time.current.to_s(:db)
-if Settings['run_cron']
+if Settings['run_cron'].to_bool
   #RunEveryValidation.check_all!
+  Alert.generate_alert_report(2.minutes)
 end
