007474fce65a9a85f722d53a0ae0ec451b5ffec6
Roman Heinrich <roman.heinrich@gmail.com>
2013-01-17 17:18:57 +0200

    plugins_for_deputy action

--- app/controllers/plugins_controller.rb
+++ app/controllers/plugins_controller.rb
@@ -11,20 +11,8 @@ class PluginsController < RestController
   index! do |format|
     format.html{render 'index'}
     format.rb do
-      address, name = Deputy.extract_address_and_name(request)
-      deputy = Deputy.find_by_address_or_name(address, name)
-      info = "(name:#{name} or address:#{address})"
-      text = if deputy
-        if deputy.deputy_plugins.empty?
-          "# there are not plugins for you #{info}"
-        else
-          "# your plugins #{info}\n" +
-          deputy.deputy_plugins(:include => :plugin).map{|dp| "##{dp.plugin.name}\n#{dp.code_for_deputy}" }.join()
-        end
-      else
-        "# you are not registered #{info}"
-      end
-      render :text => text
+      action = PluginsForDeputyAction.new(params, request)
+      render :text => action.perform
     end
   end
 
